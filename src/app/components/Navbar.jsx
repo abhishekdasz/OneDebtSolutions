cc5f8854eed99e9c110677ba70a6278cf12c0cd2
Abhishek Das <abhishekdaszy@gmail.com>
2024-11-15 21:17:44 +0530

    added homepage

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,48 @@
+'use client';
+import React, { useState } from 'react';
+import styles from '@/app/styles/navbar.module.scss';
+import { MdDarkMode } from "react-icons/md";
+import { CiLight } from "react-icons/ci";
+import Link from 'next/link';
+
+const NavBar = () => {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  }
+  return (
+    <div className={styles.navbarSection}>
+      <div className={`${styles.navbarContainer} ${isDarkMode ? styles.darkMode: ''}`}>
+
+
+        <div className={styles.logo}>
+          <Link href='/'> <img src="onedebt.png" alt="logo" /> </Link>
+        </div>
+
+
+        <div className={styles.nav}>
+          <ul>
+            <li><Link href='/'>Home</Link></li>
+            <li><Link href='/about'>About Us</Link></li>
+            <li><Link href='/services'>Services</Link></li>
+            <li><Link href='/career'>Career</Link></li>
+            <li><Link href='/contact'>Contact Us</Link></li>
+          </ul>
+        </div>
+
+
+        <div className={styles.darkMode}>
+          <button 
+            className={styles.darkModeButton} 
+            onClick={toggleDarkMode}
+            aria-label="Toggle dark mode"
+          >
+            {isDarkMode ? <CiLight /> : <MdDarkMode />}
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,33 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import React from 'react'
+import homeStyles from '@/app/page.module.scss'
 
-export default function Home() {
+const page = () => {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div 
+    className={homeStyles.homeSection} 
+    style={{
+      backgroundImage:"url('/bg.png')",
+      backgroundSize: 'cover',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center',
+    }}    
+    >
+      <div className={homeStyles.homeContainer}>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+        <div className={homeStyles.homeLeft}>
+          <div className={homeStyles.description}>
+            <h1> OneDebt Solutions </h1>
+            <p> At One Debt Solutions, we offer comprehensive debt collection services, including early-stage collections, overdue account management, and legal recovery solutions. Our expertise spans various industries, allowing us to deliver customized strategies that maximize recovery rates for our clients. With a commitment to compliance and ethical practices, we handle each case with diligence and respect. </p>
+            <button> Explore </button>
+          </div>
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+
+        <div className={homeStyles.homeRight}>
+          <img src="home.png" alt="home" />
+        </div>
+      </div>
     </div>
-  );
+  )
 }
+
+export default page
