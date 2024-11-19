73fd8d32fd0f750c82ea6812029d0f1dc0db504a
Abhishek Das <abhishekdaszy@gmail.com>
2024-11-19 23:38:24 +0530

    adding loader

---
+++ src/app/loading.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const loading = () => {
+  return (
+    <div style={{display:'flex', height:'100vh', alignItems:'center', justifyContent:'center', fontSize:'5rem', color:'red'}}>
+      ...loading
+    </div>
+  )
+}
+
+export default loading
