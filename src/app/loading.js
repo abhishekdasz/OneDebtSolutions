991ba607acea7ad1e8622abeee899228e946abe8
Abhishek Das <abhishekdaszy@gmail.com>
2024-11-19 23:42:58 +0530

    adding loader

--- src/app/loading.js
+++ src/app/loading.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const loading = () => {
   return (
-    <div style={{display:'flex', alignItems:'center', justifyContent:'center', height:'100vh', background}}>
+    <div style={{display:'flex', alignItems:'center', justifyContent:'center', height:'100vh', background:'#181818'}}>
         <div className='loader' >  </div>
     </div>
   )
